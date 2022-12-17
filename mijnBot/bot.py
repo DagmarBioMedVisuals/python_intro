8a5c0e257343c8cb702a62fb29b97b9d1d780678
DagmarBioMedVisuals <80312874+DagmarBioMedVisuals@users.noreply.github.com>
2022-12-17 17:43:36 +0100

    Nieuwe .py files

--- mijnBot/bot.py
+++ mijnBot/bot.py
@@ -10,6 +10,15 @@ load_dotenv() # laadt de informatie uit .env file
 # Setup van Python for Discord library
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = interactions.Client(token=TOKEN)
+GUILD = os.getenv('GUILD_ID')
+
+@client.command(
+    name="getstats",
+    description="Get your player stats",
+    scope=GUILD,
+)
+async def get_stats(ctx: interactions.CommandContext):
+    await ctx.send("Here are your player stats!")
 
 @client.event
 async def on_ready(): # Functie bij verbinding bot met server
---
+++ mijnBot/speler.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, id, xp=0, attack=1, hp=10):
+        self.id = id
+        self.xp = xp
+        self.attack = attack
+        self.max_hp = hp
+        self.current_hp = hp
+        
